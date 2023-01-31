75c8fce37527d66fe241f522f0e7f86d12d47751
Xin Tan <mactanxin@gmail.com>
2023-01-31 13:48:31 +0800

    修改Noice

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -26,35 +26,33 @@ require("lazy").setup({
 	},
 	{
 		"folke/noice.nvim",
-		config = function()
-			require("noice").setup({
-				lsp = {
-					message = {
-						enabled = true,
-					},
-					override = {
-						["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-						["vim.lsp.util.stylize_markdown"] = true,
-						["cmp.entry.get_documentation"] = true,
-					},
+		opts = {
+			lsp = {
+				message = {
+					enabled = true,
 				},
-				format = {
-					-- conceal: (default=true) This will hide the text in the cmdline that matches the pattern.
-					-- view: (default is cmdline view)
-					-- opts: any options passed to the view
-					-- icon_hl_group: optional hl_group for the icon
-					-- title: set to anything or empty string to hide
-					cmdline = { pattern = "^:", icon = "", lang = "vim" },
-					search_down = { kind = "search", pattern = "^/", icon = " ", lang = "regex" },
-					search_up = { kind = "search", pattern = "^%?", icon = " ", lang = "regex" },
-					filter = { pattern = "^:%s*!", icon = "$", lang = "bash" },
-					lua = { pattern = "^:%s*lua%s+", icon = "", lang = "lua" },
-					help = { pattern = "^:%s*he?l?p?%s+", icon = "" },
-					input = {}, -- Used by input()
-					-- lua = false, -- to disable a format, set to `false`
+				override = {
+					["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+					["vim.lsp.util.stylize_markdown"] = true,
+					["cmp.entry.get_documentation"] = true,
 				},
-			})
-		end,
+			},
+			format = {
+				-- conceal: (default=true) This will hide the text in the cmdline that matches the pattern.
+				-- view: (default is cmdline view)
+				-- opts: any options passed to the view
+				-- icon_hl_group: optional hl_group for the icon
+				-- title: set to anything or empty string to hide
+				cmdline = { pattern = "^:", icon = "", lang = "vim" },
+				search_down = { kind = "search", pattern = "^/", icon = " ", lang = "regex" },
+				search_up = { kind = "search", pattern = "^%?", icon = " ", lang = "regex" },
+				filter = { pattern = "^:%s*!", icon = "$", lang = "bash" },
+				lua = { pattern = "^:%s*lua%s+", icon = "", lang = "lua" },
+				help = { pattern = "^:%s*he?l?p?%s+", icon = "" },
+				input = {}, -- Used by input()
+				-- lua = false, -- to disable a format, set to `false`
+			},
+		},
 		dependencies = {
 			"MunifTanjim/nui.nvim",
 			"rcarriga/nvim-notify",
@@ -335,7 +333,7 @@ require("lazy").setup({
 			{ "<leader>la", "<cmd>Lspsaga code_action<CR>", "n, v" },
 			{ "gpd", "<cmd>Lspsaga peek_definition<CR>" },
 			{ "<leader>lsd", "<cmd>Lspsaga show_line_diagnostics<CR>" },
-			{ "<leader>so", "<cmd>LSoutlineToggle<CR>" },
+			{ "<leader>so", "<cmd>Lspsaga outline<CR>" },
 			{ "K", "<cmd>Lspsaga hover_doc<CR>" },
 		},
 	},
