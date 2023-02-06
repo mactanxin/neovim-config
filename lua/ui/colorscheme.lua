f2974a6c2c33c44a028cab65bbffec303c064483
Xin Tan <mactanxin@gmail.com>
2023-02-06 11:08:48 +0800

    index of colorscheme

--- lua/ui/colorscheme.lua
+++ lua/ui/colorscheme.lua
@@ -1,15 +1,12 @@
 local function chooseRandomColor()
-  local myColorTable = {
-    "zephyr",
-    "ui.gruvbox",
-    "ui.ayu"
-  }
+	local myColorTable = {
+		"zephyr",
+		"ui.gruvbox",
+		"ui.ayu",
+	}
 	local rand = math.random(#myColorTable)
 	return myColorTable[rand]
 end
 
 local randColor = chooseRandomColor()
---[[ require('zephyr') ]]
---[[ require('ui.gruvbox') ]]
-require('ui.ayu')
-require('lualine').setup()
+require("ui.ayu")
