792cda92570e525db804d9f235d5a5adada40c0b
Xin Tan <mactanxin@gmail.com>
2023-02-22 17:47:07 +0800

    feat(ui): 删除随机颜色

--- lua/ui/init.lua
+++ lua/ui/init.lua
@@ -1,16 +1,2 @@
-local function chooseRandomColor()
-	local myThemeTable = {
-		"ui.colors.gruvbox_colors",
-		"ui.colors.purple-green",
-		"ui.colors.emerald",
-		"ui.colors.ember-fire",
-	}
-	local rand = math.random(#myThemeTable)
-	return myThemeTable[rand]
-end
-
---[[ require("ui.startup-screen") ]]
 require("ui.dashboard")
 require("ui.colorscheme")
---[[ require("ui.colors.ember-fire") ]]
--- require(chooseRandomColor())
