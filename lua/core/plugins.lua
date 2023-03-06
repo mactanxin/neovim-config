17d5eb59f73e347fa30dced2271f50aeebc47a74
Xin Tan <mactanxin@gmail.com>
2023-03-06 16:15:59 +0800

    delete useless codee

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -386,14 +386,6 @@ require("lazy").setup({
     dependencies = {
       { "roobert/tailwindcss-colorizer-cmp.nvim", config = true },
     },
-    -- opts = function(_, opts)
-    --   -- original LazyVim kind icon formatter
-    --   local format_kinds = opts.formatting.format
-    --   opts.formatting.format = function(entry, item)
-    --     format_kinds(entry, item) -- add icons
-    --     return require("tailwindcss-colorizer-cmp").formatter(entry, item)
-    --   end
-    -- end,
   },
   {
     "glepnir/lspsaga.nvim",
