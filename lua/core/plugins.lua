07c52b2d23202d0b325a54073d1747b6fc9f2129
Xin Tan <mactanxin@gmail.com>
2023-06-17 16:07:57 +0800

    🚀 feat(plugins.lua): add paint.nvim plugin and configure it to highlight @something in comments with Constant color in lua files

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -62,6 +62,24 @@ require("lazy").setup({
     },
   },
   {
+    "folke/paint.nvim",
+    config = function()
+      require("paint").setup({
+        ---@type PaintHighlight[]
+        highlights = {
+          {
+            -- filter can be a table of buffer options that should match,
+            -- or a function called with buf as param that should return true.
+            -- The example below will paint @something in comments with Constant
+            filter = { filetype = "lua" },
+            pattern = "%s*%-%-%-%s*(@%w+)",
+            hl = "Constant",
+          },
+        },
+      })
+    end,
+  },
+  {
     "rcarriga/nvim-notify",
     opts = {
       background_colour = "#000000"
