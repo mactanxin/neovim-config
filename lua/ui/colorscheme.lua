9ffd4b85116aa62d89983619625f202f7ad8b0cc
Xin Tan <mactanxin@gmail.com>
2023-06-26 10:29:57 +0800

    🎨 style(colorscheme.lua): fix indentation and whitespace issues in chooseRandomColor function

--- lua/ui/colorscheme.lua
+++ lua/ui/colorscheme.lua
@@ -1,11 +1,11 @@
 local function chooseRandomColor()
-	local myColorTable = {
-		"zephyr",
-		"ui.gruvbox",
-		"ui.ayu",
-	}
-	local rand = math.random(#myColorTable)
-	return myColorTable[rand]
+  local myColorTable = {
+    "zephyr",
+    "ui.gruvbox",
+    "ui.ayu",
+  }
+  local rand = math.random(#myColorTable)
+  return myColorTable[rand]
 end
 
 local randColor = chooseRandomColor()
