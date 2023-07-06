0840b726f38b5ce398d7191822f51e3623458e53
Xin Tan <mactanxin@gmail.com>
2023-07-06 16:17:18 +0800

    plugin(saga): add goto_definition & type_definition

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -483,10 +483,21 @@ augroup END
       { "<leader>lr",  "<cmd>Lspsaga rename<CR>" },
       { "<leader>la",  "<cmd>Lspsaga code_action<CR>",          "n, v" },
       { "gpd",         "<cmd>Lspsaga peek_definition<CR>" },
+      { "gd",          "<cmd>Lspsaga goto_definition<CR>" },
+      { "gpt",         "<cmd>Lspsaga peek_type_definition<CR>" },
+      { "gt",          "<cmd>Lspsaga goto_type_definition<CR>" },
+      { "<Leader>lci", "<cmd>Lspsaga incoming_calls<CR>" },
+      { "<Leader>lco", "<cmd>Lspsaga outgoing_calls<CR>" },
       { "<leader>lsd", "<cmd>Lspsaga show_line_diagnostics<CR>" },
       { "<leader>so",  "<cmd>Lspsaga outline<CR>" },
       { "K",           "<cmd>Lspsaga hover_doc<CR>" },
     },
+    opts = {
+      beacon = {
+        enable = true,
+        frequency = 7,
+      },
+    }
   },
   -- treesitter
   {
